fb53d25add67c05f7040cdce232a24a8c6e53ce4
Samantha Ambroise <ilkigin@gmail.com>
2019-04-18 21:04:55 -0400

    added cors and validation

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const MOVIEDEX = require('./movie-data-small.json')
+
+
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
+
+function validateBearerToken(req, res, next){
+    
+    console.log(process.env.API_TOKEN)
+    debugger
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request!'})
+    }
+    next()
+}
+app.use(validateBearerToken)
+
+
 
 function handleGenreCountryVote(req, res) {
     const { genre, country, avg_vote } = req.query;
